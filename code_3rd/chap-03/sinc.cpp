a1e84d19a9ef4c8104cc870bf1dfcbc1c192db2b
hexu <hexu_1985@sina.com>
2022-12-26 18:13:00 +0800

    add poly_line*.cpp

--- code_3rd/chap-03/sinc.cpp
+++ code_3rd/chap-03/sinc.cpp
@@ -1,9 +1,9 @@
-#include <windows.h>
-#include <iostream.h>
+//#include <windows.h>
+#include <iostream>
 #include <math.h>
-#include <gl/GL.h>
-#include <gl/GLU.h>
-#include <gl/GLUT.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 
 // use a named constant to approximate pi
 const float pi = 3.14159265358979;
@@ -13,19 +13,17 @@ const float pi = 3.14159265358979;
 void setWindow(GLfloat left, GLfloat right, GLfloat bottom, GLfloat top)
 
 {
-            glMatrixMode(GL_PROJECTION);
-            glLoadIdentity();
-            gluOrtho2D(left, right, bottom, top);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(left, right, bottom, top);
 }
 
 
 //---------------- setViewport ------------------
-void setViewport(int left, int right, int width, int height)
+void setViewport(int left, int right, int bottom, int top)
 // define our own function to specify the viewport, as explained later
 {
-            GLfloat bottom = 0.5;
-            GLfloat top = 1.5;
-            glViewport(left, bottom, right - left, top - bottom);
+    glViewport(left, bottom, right - left, top - bottom);
 }
 
 //<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -33,46 +31,43 @@ void setViewport(int left, int right, int width, int height)
 void myDisplay(void) // plot the sinc function, using world coordinates
 
 {
-		glClear( GL_COLOR_BUFFER_BIT );
-            glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();
-            glBegin(GL_LINE_STRIP);
+    glClear( GL_COLOR_BUFFER_BIT );
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glBegin(GL_LINE_STRIP);
 /* automatically uses the defined window and viewport, 
 clips properly, and performs the proper mapping */
 
-		for(
-GLfloat x = -4.0; x < 4.0; x += 0.1)     // draw the plot
-                   glVertex2f(x, sin(pi * x) / (pi * x));
+        for(GLfloat x = -4.0; x < 4.0; x += 0.1)     // draw the plot
+            glVertex2f(x, sin(pi * x) / (pi * x));
 
-            glEnd();
-            glFlush();
+    glEnd();
+    glFlush();
 }
 
  
 
 //<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 void myInit(void)
-
 {
-            glClearColor(1.0,1.0,1.0,0.0);
-            glColor3f(0.0f,0.0f,1.0f);
-            glLineWidth(1.0);
+    glClearColor(1.0,1.0,1.0,0.0);
+    glColor3f(0.0f,0.0f,1.0f);
+    glLineWidth(1.0);
 }
 
 //<<<<<<<<<<<<<<main>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-void main(int argc, char** argv)
-
+int main(int argc, char** argv)
 {
-            glutInit(&argc, argv);
-            glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-            glutInitWindowSize(640,480);
-            glutInitWindowPosition(100,150);
-            glutCreateWindow("Sinc Function");
-            glutDisplayFunc(myDisplay);
-            myInit( );
-			// call the function to specify the window
-		    setWindow(-5.0, 5.0, -0.3, 1.0);
-            // call the function to specify the viewport
-			setViewport(0, 640, 0, 480);
-			glutMainLoop( );
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(100,150);
+    glutCreateWindow("Sinc Function");
+    glutDisplayFunc(myDisplay);
+    myInit( );
+    // call the function to specify the window
+    setWindow(-5.0, 5.0, -0.3, 1.0);
+    // call the function to specify the viewport
+    setViewport(0, 640, 0, 480);
+    glutMainLoop( );
 }
